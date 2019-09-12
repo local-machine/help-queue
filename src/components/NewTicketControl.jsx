bb5e90ee122d83a09be6df8e558bb188d355b99e
Student <example@example.com>
2019-09-11 17:30:33 -0700

    It will have application state functionality

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ConfirmationQuestions from './ConfirmationQuestions';
 import NewTicketForm from './NewTicketForm';
+import PropTypes from 'prop-types';
 
 class NewTicketControl extends React.Component {
 
@@ -12,16 +13,16 @@ class NewTicketControl extends React.Component {
     this.handleTroubleshootingConfirmation = this.handleTroubleshootingConfirmation.bind(this);
   }
 
-  handleTroubleshootingConfirmation(){
-    this.setState({formVisibleOnPage: true});
+  handleTroubleshootingConfirmation() {
+    this.setState({ formVisibleOnPage: true });
   }
 
   render() {
     let currentlyVisibleContent = null;
-    if (this.state.formVisibleOnPage){
-      currentlyVisibleContent = <NewTicketForm />;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleContent = <NewTicketForm onNewTicketCreation={this.props.onNewTicketCreation} />;
     } else {
-      currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation}/>;
+      currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation} />;
     }
     return (
       <div>
@@ -31,5 +32,8 @@ class NewTicketControl extends React.Component {
   }
 }
 
-export default NewTicketControl;
+NewTicketControl.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
+export default NewTicketControl;
